f833e4075129734b4aeeebb6ca9622703febc6bd
Wai-Shing Luk <luk036@gmail.com>
2023-03-03 10:30:48 +0800

    separate lds and sphere-n

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,7 @@ pub mod lds_n;
 
 pub use crate::lds::PRIME_TABLE;
 pub use crate::lds::{Circle, Halton, Sphere, Sphere3Hopf, Vdcorput};
-pub use crate::lds_n::{CylinN, CylindN, HaltonN, NSphere, Sphere3, SphereN};
-pub use crate::lds_n::{Cylind, SphereGen}; // Traits
+pub use crate::lds_n::HaltonN;
 
 #[cfg(test)]
 mod tests {
@@ -56,14 +55,6 @@ mod tests {
         let res = s3fgen.pop();
         assert_approx_eq!(res[0], 0.23764785962349413);
 
-        let mut sgen = Sphere3::new(&base);
-        sgen.reseed(10);
-        for _i in 0..10 {
-            println!("{:?}", sgen.pop());
-        }
-        let res = sgen.pop();
-        assert_approx_eq!(res[0], 0.3430622238280562);
-
         let mut hgen = HaltonN::new(5, &PRIME_TABLE);
         hgen.reseed(10);
         for _i in 0..10 {
@@ -71,37 +62,5 @@ mod tests {
         }
         let res = hgen.pop_vec();
         assert_approx_eq!(res[0], 0.65625);
-
-        let mut cgen = CylinN::new(5, &PRIME_TABLE);
-        cgen.reseed(0);
-        for _i in 0..10 {
-            println!("{:?}", cgen.pop_vec());
-        }
-        let res = cgen.pop_vec();
-        assert_approx_eq!(res[0], 0.032662755534715766);
-
-        let mut cgen = CylindN::new(5, &PRIME_TABLE);
-        cgen.reseed(0);
-        for _i in 0..10 {
-            println!("{:?}", cgen.pop_vec());
-        }
-        let res = cgen.pop_vec();
-        assert_approx_eq!(res[0], 0.032662755534715766);
-
-        let mut sgen = SphereN::new(4, &PRIME_TABLE);
-        sgen.reseed(0);
-        for _i in 0..10 {
-            println!("{:?}", sgen.pop_vec());
-        }
-        let res = sgen.pop_vec();
-        assert_approx_eq!(res[0], 0.006903401092767657);
-
-        let mut sgen = NSphere::new(4, &PRIME_TABLE);
-        sgen.reseed(0);
-        for _i in 0..10 {
-            println!("{:?}", sgen.pop_vec());
-        }
-        let res = sgen.pop_vec();
-        assert_approx_eq!(res[0], 0.006903401092767657);
     }
 }
